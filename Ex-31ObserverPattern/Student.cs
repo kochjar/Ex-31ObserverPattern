e118af99319e3af51a7781292f056c58fead9151
kochjar <timgfx@outlook.com>
2023-05-16 21:18:10 +0200

    Øvelse 4 done

---
+++ Ex-31ObserverPattern/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_31ObserverPattern
+{
+    public class Student : Observer
+    {
+        private Academy subject;
+
+        public string Name { get; }
+
+        public string Message { get; set; }
+
+        public Student(Academy subject, string name)
+        {
+            this.subject = subject;
+            this.Name = name;
+            this.Message = subject.Message;
+        }
+
+        public override void Update()
+        {
+            this.Message = this.subject.Message;
+            Console.WriteLine($"Studerende {Name} modtog nyheden '{this.Message}' fra akademiet {this.subject.Name}");
+        }
+    }
+}
